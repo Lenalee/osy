3d4eeb2f8fba94a476c274f7f3f3c814cb7995a6
Jana Arameleva <zajchig@gmail.com>
2019-10-13 19:25:50 +0200

    Add test data

--- data/include.h
+++ data/include.h
@@ -5,9 +5,17 @@
  #include "strdio.h"
 #include "dsfds.h>
 
-toto_je_funkce_2_priklad()
+int toto_je_funkce_2_priklad()
+int toto_je_funkce_2_priklad() # toto_je_funkce_2_priklad
+  int toto_je_funkce_2_priklad()
+  int   toto_je_funkce_2_priklad()
+ toto_je_funkce_2_priklad()
+toto_je_funkce_2_priklad
 _func()
 _func_xy()
 sin_Cos()
 remove_allX()
 internal__func()
+int some_function();
+int this_is1_some_2_function ( void );
+/* function_call(arg); */
