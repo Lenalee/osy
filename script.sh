22fc587500ef25cd74af17a727e23b6a57544017
Jana Arameleva <zajchig@gmail.com>
2019-10-13 20:13:50 +0200

    Working version

--- script.sh
+++ script.sh
@@ -1,41 +1,26 @@
-#!/bin/bash -ae
-
-PREFIX=
-REGEX=
+#!/bin/bash
 
 function print_help () {
 	echo "Usage: $0 [-h] -i prefix -r -f regex"
 }
 
-function do_replace () {
-	sed "s|\([[:space:]]*#[[:space:]]*include[[:space:]]*<\)\(.*>\)|\1$PREFIX\2|g" | sed "s|\([[:space:]]*#[[:space:]]*include[[:space:]]*\"\)\(.*\"\)|\1$PREFIX\2|g" 
-}
-
-function do_camel () { 
-	sed -r -e ':loop
-/\b[[:lower:][:digit:]]+(_[[:lower:][:digit:]]+)*\(/{s/(^|_)([a-z0-9])/\U\2/g
-b loop}'
-}
-
 function replace_include () {
-	[ -z "$1" ] && (do_replace; exit 0)
-
 	for file in $@
 	do
-		TMP=$(mktemp)
-		cat $file | do_replace > $TMP
-		mv $TMP $file
+		sed -i -e "s|\([[:space:]]*#[[:space:]]*include[[:space:]]*<\)\(.*>\)|\1$PREFIX\2|g" -e"s|\([[:space:]]*#[[:space:]]*include[[:space:]]*\"\)\(.*\"\)|\1$PREFIX\2|g" $file
 	done
 }
 
 function replace_camel () {
-	[ -z "$1" ] && (do_camel; exit 0)
-
 	for file in $@
 	do
-		TMP=$(mktemp)
-		cat $file | do_camel > $TMP
-		mv $TMP $file
+		while true
+		do
+			name=$(grep -m 1 -Eo '\b[[:lower:][:digit:]]+(_[[:lower:][:digit:]]+)*[[:space:]]*\(' "$file")
+			[ -z "$name" ] && break
+			subst=$(echo "$name" | sed -r 's/(^|_)([a-z0-9])/\U\2/g')
+			eval "sed -i 's/$name/$subst/g' \"$file\""
+		done
 	done
 }
 
@@ -64,7 +49,19 @@ do
 done
 
 shift $((OPTIND-1))
-[ ! -z "$PREFIX" ] && replace_include "$@"
-[ ! -z "$CAMEL" ] && replace_camel "$@"
+
+if [ -z "$1" ]
+then
+	TMP=$(mktemp)
+	cat /dev/stdin > $TMP
+	FILES="$TMP"
+else
+	FILES="$@"
+fi
+
+[ ! -z "$PREFIX" ] && replace_include "$FILES"
+[ ! -z "$CAMEL" ] && replace_camel "$FILES"
+
+[ ! -z "$TMP" ] && cat $TMP
 
 exit 0
