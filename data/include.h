329eb05409aa27b9b28e6a3157e1c4abdcf65d17
Jana Arameleva <zajchig@gmail.com>
2019-10-14 23:18:22 +0200

    Everything working, FINAL VERSE

--- data/include.h
+++ data/include.h
@@ -1,22 +1,25 @@
-# include <String.h>
-#include <String.h>
-#include   <String.h>
-  #include   <String.h>
- #include "strdio.h"
+# include <hvonohvonohvonoString.h>
+#include <hvonohvonohvonoString.h>
+#include   <hvonohvonohvonoString.h>
+  #include   <hvonohvonohvonoString.h>
+ #include "hvonohvonohvonostrdio.h"
 #include "dsfds.h>
 
-int toto_je_funkce_2_priklad()
-int toto_je_funkce_2_priklad() # toto_je_funkce_2_priklad
-  int toto_je_funkce_2_priklad()
-  int   toto_je_funkce_2_priklad()
- toto_je_funkce_2_priklad()
+int totoJeFunkce2Priklad()
+int totoJeFunkce2Priklad() # toto_je_funkce_2_priklad
+  int totoJeFunkce2Priklad()
+  int   totoJeFunkce2Priklad()
+ totoJeFunkce2Priklad()
 toto_je_funkce_2_priklad
 _func()
 _func_xy()
 sin_Cos()
 remove_allX()
 internal__func()
-int some_function();
-int this_is1_some_2_function ( void );
-/* function_call(arg); */
-void func_2(){int tmp_var; function_call(0);}
+int someFunction();
+int thisIs1Some2Function ( void );
+/* functionCall(arg); */
+void func2(){int tmp_var; functionCall(0);}
+void myFunc( int i );
+void alsoMyFunc( int i );
+void fooFunc( int i );
--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+F=0;
+
 function print_help () {
 	echo "Usage: $0"
 	echo "-h help"
@@ -11,23 +13,27 @@ function print_help () {
 function replace_include () {
 	for file in "$@"
 	do
-		sed -i -e "s|\([[:space:]]*#[[:space:]]*include[[:space:]]*<\)\(.*>\)|\1$PREFIX\2|g" -e"s|\([[:space:]]*#[[:space:]]*include[[:space:]]*\"\)\(.*\"\)|\1$PREFIX\2|g" "$file"
+		sed -i -e "s|\([[:space:]]*#[[:space:]]*include[[:space:]]*<\)\(.*>\)|\1$PREFIX\2|g" -e"s|\([[:space:]]*#[[:space:]]*include[[:space:]]*\"\)\(.*\"\)|\1$PREFIX\2|g" $file
 	done
 }
 
 function replace_camel () {
-	for file in "$@"
+
+	echo "$REGEX" >&2;
+	for file in $@
 	do
 		names=$(grep -Eo '\b[[:lower:][:digit:]]+(_[[:lower:][:digit:]]+)*[[:space:]]*\(' "$file" | uniq | grep -E "$REGEX")
 		for name in $names
 		do
 			subst=$(echo $name | sed -r 's/(_)([a-z0-9])/\U\2/g')
-			#1>&2 echo "replacing name:$name to subst:$subst in file:$file"
-			eval "sed -i 's/$name/$subst/g' \"$file\""
+			if [ "$F" -eq 1 ]; then
+				eval "sed -i 's/ $name/ $subst/g' \"$file\""
+			else
+				eval "sed -i 's/$name/$subst/g' \"$file\""
+			fi
 		done
 	done
 }
-
 
 while getopts "h?i:rf:" opt
 do
@@ -42,8 +48,8 @@ do
 	r)
 		CAMEL=true
 		;;
-	f)
-		REGEX="$OPTARG"
+	f)	F=1;
+		REGEX="${OPTARG}"
 		;;
 	*)
 		print_help
@@ -64,10 +70,8 @@ else
 fi
 
 [ -z "$REGEX" ] && REGEX=.*
-
 [ ! -z "$PREFIX" ] && replace_include "$FILES"
 [ ! -z "$CAMEL" ] && replace_camel "$FILES"
-
 [ ! -z "$TMP" ] && cat "$TMP"
 
 exit 0
